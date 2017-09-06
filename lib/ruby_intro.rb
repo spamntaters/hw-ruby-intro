f11f3201042121102ef8a6b1c8a74c9f07533e30
Samuel Murray <smurra28@uncc.edu>
2017-09-06 18:23:32 -0400

    Complete Part 3

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -43,4 +43,18 @@ end
 
 class BookInStock
 # YOUR CODE HERE
+  def initialize(isbn, price)
+    raise ArgumentError.new("Not valid ISBN Number") if isbn.length == 0
+    raise ArgumentError.new("Not valid number") if price <= 0
+    @isbn = isbn
+    @price = price
+  end
+
+  attr_accessor :isbn
+  attr_accessor :price
+
+  def price_as_string
+    return "$" + sprintf("%.2f", @price)
+  end
+
 end
